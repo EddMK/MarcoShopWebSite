86426f04e47c0a3700e088c5d689edbbbe0b170b
Malke Kourie <malkekourie@hotmail.com>
2025-10-01 20:39:04 +0200

    progress

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeLanguage() {
+  document.getElementById("selectionFlag").style.display = "none";
+}
+
+function openDialogToChangeLanguage(){
+    document.getElementById("selectionFlag").style.display = "block";
+}
